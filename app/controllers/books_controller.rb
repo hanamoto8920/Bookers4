e11d39d3c726a11ab81a14cd53b90982dc96504f
EC2 Default User <ec2-user@ip-172-31-64-135.ec2.internal>
2020-09-17 07:22:12 +0000

    start

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id), notice: "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -16,6 +20,26 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @new = Book.new
+    
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    
+    if @book.save
+     redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+     render :edit
+    end
+     
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get 'books/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  resources :books, only: [:edit, :create, :index, :show]
+  
+  resources :books, only: [:edit, :create, :index, :show, :update, :destroy]
 
 end
