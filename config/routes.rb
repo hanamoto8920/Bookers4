92a6fbfc5140e51322ce18d503086d8d5e302b3f
EC2 Default User <ec2-user@ip-172-31-64-135.ec2.internal>
2020-09-17 10:15:04 +0000

    二回目

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get 'books/create'
   # get 'books/edit'
   # get 'books/show'
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
