b10e676491195819deae6a125be485ee9e098dd7
EC2 Default User <ec2-user@ip-172-31-64-135.ec2.internal>
2020-09-18 12:29:29 +0000

    三回目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
     redirect_to book_path(@book.id), notice: "Book was successfully created."
     else
@@ -21,19 +22,19 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @new = Book.new
-    
+
   end
 
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    
+
     if @book.save
      redirect_to book_path(@book.id), notice: "Book was successfully updated."
     else
      render :edit
     end
-     
+
   end
 
   def destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_095802) do
+ActiveRecord::Schema.define(version: 2020_09_18_052116) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "user_images", force: :cascade do |t|
+    t.string "image_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
